8c16bd82011c5d6f10096f51e8bc84eaba3a5a22
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-04-03 14:29:02 +0530

    error handle

--- src/app/dashboard/component/news-list/news-list.component.ts
+++ src/app/dashboard/component/news-list/news-list.component.ts
@@ -1,4 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { environment } from 'src/environment/enviroment';
@@ -7,40 +11,57 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
-  styleUrls: ['./news-list.component.scss']
+  styleUrls: ['./news-list.component.scss'],
 })
-export class NewsListComponent implements OnInit{
-  constructor(private http:HttpClient, private updateService:NewsUpdateService, private route:Router,private toastr:ToastrService) {}
-ngOnInit(): void {
-    this.getListofNews()
-}
-  listOfNews:any
+export class NewsListComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private updateService: NewsUpdateService,
+    private route: Router,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit(): void {
+    this.getListofNews();
+  }
+  listOfNews: any;
 
-  getListofNews(){
-    this.http.get(environment.baseUrl+'news').subscribe((res:any)=>{
-      this.listOfNews=res.data;
-    })
+  getListofNews() {
+    this.http.get(environment.baseUrl + 'news').subscribe((res: any) => {
+      this.listOfNews = res.data;
+    });
   }
   getFormattedDate(timestamp: string): string {
     const date = new Date(timestamp);
-    const options:any = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const options: any = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
     return date.toLocaleDateString('en-US', options); // Adjust locale and options as needed
   }
-  deleteNews(id:any){
-    this.http.delete(environment.baseUrl+'news/'+id).subscribe((res:any)=>{
-      // console.log(res)
-      if(res.message=="News deleted successfully"){
-        // location.reload()
-        this.toastr.success("News Deleted Successfully")
-        this.getListofNews()
+  deleteNews(id: any) {
+    this.http.delete(environment.baseUrl + 'news/' + id).subscribe(
+      (res: any) => {
+        // console.log(res)
+        if (res.success) {
+          // location.reload()
+          this.toastr.success('News Deleted Successfully');
+          this.getListofNews();
+        }
+      },
+      (error: HttpErrorResponse) => {
+        console.log('error', error);
+        this.toastr.error(error.error.message);
       }
-    })
+    );
   }
-  updateNews(news:any){
-    console.log(news)
-    this.updateService.news=news
-    if(news){
-      this.route.navigate(['/','news-update'])
+  updateNews(news: any) {
+    console.log(news);
+    this.updateService.news = news;
+    if (news) {
+      this.route.navigate(['/', 'news-update']);
     }
   }
 }
--- src/app/dashboard/component/news-update/news-update.component.ts
+++ src/app/dashboard/component/news-update/news-update.component.ts
@@ -1,7 +1,21 @@
-import { Component,ViewChild,ElementRef,AfterViewInit, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import { AngularEditorConfig, UploadResponse } from '@kolkov/angular-editor';
-import { HttpClient, HttpEvent, HttpEventType, HttpRequest, HttpResponse } from '@angular/common/http';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpEventType,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { environment } from 'src/environment/enviroment';
 import { NewsUpdateService } from 'src/app/services/update.service';
 import { Router } from '@angular/router';
@@ -10,131 +24,136 @@ import { Observable, Observer, of, tap } from 'rxjs';
 @Component({
   selector: 'app-news-update',
   templateUrl: './news-update.component.html',
-  styleUrls: ['./news-update.component.scss']
+  styleUrls: ['./news-update.component.scss'],
 })
-export class NewsUpdateComponent implements OnInit, OnDestroy{
+export class NewsUpdateComponent implements OnInit, OnDestroy {
   @ViewChild('angularEditor') editor!: ElementRef;
   // content:any;
   // title:any
-  images:any[]=[]
+  images: any[] = [];
   newsContent = new FormGroup({
-    content:new FormControl("",Validators.required),
-    title:new FormControl("",Validators.required)
-  })
-  updateNews:boolean=false;
-  newsId!:string;
-  constructor(private http:HttpClient,private updateService:NewsUpdateService,private route:Router,private toastr:ToastrService) {}
-   ngOnInit(): void {
-    let data:any = this.updateService.news
-    this.newsId=data._id
-    if(this.newsId){
-      console.log(data,"hii")
-      this.updateNews=true
+    content: new FormControl('', Validators.required),
+    title: new FormControl('', Validators.required),
+  });
+  updateNews: boolean = false;
+  newsId!: string;
+  constructor(
+    private http: HttpClient,
+    private updateService: NewsUpdateService,
+    private route: Router,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit(): void {
+    let data: any = this.updateService.news;
+    this.newsId = data._id;
+    if (this.newsId) {
+      console.log(data, 'hii');
+      this.updateNews = true;
       this.newsContent.patchValue({
-        content:data.content,
-        title:data.title
-      })
+        content: data.content,
+        title: data.title,
+      });
     }
   }
 
-
-config: AngularEditorConfig = {
+  config: AngularEditorConfig = {
     editable: true,
     spellcheck: true,
     minHeight: '20rem',
     maxHeight: '20rem',
-    minWidth:'100%',
-    width:'50rem',
+    minWidth: '100%',
+    width: '50rem',
 
     upload: (file: File): Observable<HttpEvent<UploadResponse>> => {
-      console.log("file is", file);
-      return Observable.create((observer: Observer<HttpEvent<UploadResponse>>) => {
-        const maxDimension = 250; // Maximum width or height for the resized image
-    
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        const image = new Image();
-        image.src = URL.createObjectURL(file);
-    
-        image.onload = () => {
-          let width = image.width;
-          let height = image.height;
-    
-          // Resize the image if either dimension is greater than the maximum
-          if (width > maxDimension || height > maxDimension) {
-            // Calculate the new dimensions while maintaining aspect ratio
-            if (width > height) {
-              height *= maxDimension / width;
-              width = maxDimension;
-            } else {
-              width *= maxDimension / height;
-              height = maxDimension;
-            }
-          }
-    
-          // Set canvas dimensions
-          canvas.width = width;
-          canvas.height = height;
-    
-          // Draw image on canvas with new dimensions
-          ctx?.drawImage(image, 0, 0, width, height);
-    
-          // Convert canvas content to Blob
-          canvas.toBlob((blob) => {
-            if (!blob) {
-              observer.error('Failed to resize image');
-              return;
-            }
-    
-            // Create FormData and append resized image
-            const formData = new FormData();
-            formData.append('file', blob, file.name);
-    
-            // Upload resized image
-            fetch(`${environment.baseUrl}news/upload`, {
-              method: 'POST',
-              body: formData
-            })
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Failed to upload file');
+      console.log('file is', file);
+      return Observable.create(
+        (observer: Observer<HttpEvent<UploadResponse>>) => {
+          const maxDimension = 250; // Maximum width or height for the resized image
+
+          const canvas = document.createElement('canvas');
+          const ctx = canvas.getContext('2d');
+          const image = new Image();
+          image.src = URL.createObjectURL(file);
+
+          image.onload = () => {
+            let width = image.width;
+            let height = image.height;
+
+            // Resize the image if either dimension is greater than the maximum
+            if (width > maxDimension || height > maxDimension) {
+              // Calculate the new dimensions while maintaining aspect ratio
+              if (width > height) {
+                height *= maxDimension / width;
+                width = maxDimension;
+              } else {
+                width *= maxDimension / height;
+                height = maxDimension;
               }
-              return response.json() as Promise<UploadResponse>;
-            })
-            .then((data: any) => {
-              console.log("data", data);
-              this.images.push(data);
-              let imageUrl = `${environment.baseUrl}${data.data.path}`;
-              console.log("data", imageUrl);
-              const httpResponse: HttpResponse<UploadResponse> = new HttpResponse({
-                body: { imageUrl },
-                status: 200,
-                statusText: 'OK',
-                // headers: null 
-              });
-              // Emit the HttpResponse
-              observer.next(httpResponse);
-              observer.complete();
-            })
-            .catch(error => {
-              console.error('Error uploading file:', error);
-              observer.error('Failed to upload file');
-            });
-          }, file.type);
-        };
-      });
+            }
+
+            // Set canvas dimensions
+            canvas.width = width;
+            canvas.height = height;
+
+            // Draw image on canvas with new dimensions
+            ctx?.drawImage(image, 0, 0, width, height);
+
+            // Convert canvas content to Blob
+            canvas.toBlob((blob) => {
+              if (!blob) {
+                observer.error('Failed to resize image');
+                return;
+              }
+
+              // Create FormData and append resized image
+              const formData = new FormData();
+              formData.append('file', blob, file.name);
+
+              // Upload resized image
+              fetch(`${environment.baseUrl}news/upload`, {
+                method: 'POST',
+                body: formData,
+              })
+                .then((response) => {
+                  if (!response.ok) {
+                    this.toastr.error("error while upload image")
+                    throw new Error('Failed to upload file');
+                  }
+                  return response.json() as Promise<UploadResponse>;
+                })
+                .then((data: any) => {
+                  console.log('data', data);
+                  this.images.push(data);
+                  let imageUrl = `${environment.baseUrl}${data.data.path}`;
+                  console.log('data', imageUrl);
+                  const httpResponse: HttpResponse<UploadResponse> =
+                    new HttpResponse({
+                      body: { imageUrl },
+                      status: 200,
+                      statusText: 'OK',
+                      // headers: null
+                    });
+                  // Emit the HttpResponse
+                  observer.next(httpResponse);
+                  observer.complete();
+                })
+                .catch((error) => {
+                  console.error('Error uploading file:', error);
+                  observer.error('Failed to upload file');
+                });
+            }, file.type);
+          };
+        }
+      );
     },
-    
-    
-    
 
     // upload: (file: File): Observable<HttpEvent<UploadResponse>> => {
     //   console.log("file is",file)
     //   return Observable.create((observer: Observer<HttpEvent<UploadResponse>>) => {
-        
+
     //     const formData = new FormData();
     //     formData.append('file', file);
-        
+
     //     fetch(`${environment.baseUrl}news/upload`, {
     //       method: 'POST',
     //       body: formData
@@ -152,9 +171,9 @@ config: AngularEditorConfig = {
     //       console.log("data", imageUrl)
     //       const httpResponse: HttpResponse<UploadResponse> = new HttpResponse({
     //         body: {imageUrl},
-    //         status: 200, 
-    //         statusText: 'OK', 
-    //         // headers: null 
+    //         status: 200,
+    //         statusText: 'OK',
+    //         // headers: null
     //       });
     //       // Emit the HttpResponse
     //       observer.next(httpResponse);
@@ -166,7 +185,6 @@ config: AngularEditorConfig = {
     //     });
     //   });
     // },
-
 
     // upload: (file: File): Observable<HttpEvent<UploadResponse>> => {
     //   return Observable.create((observer: Observer<HttpEvent<UploadResponse>>) => {
@@ -195,36 +213,51 @@ config: AngularEditorConfig = {
     toolbarPosition: 'top',
   };
   submitContent() {
+    if (this.newsContent.valid) {
+      console.log(this.newsContent.value);
+      if (this.updateNews) {
+        this.http
+          .patch(
+            environment.baseUrl + 'news/' + this.newsId,
+            this.newsContent.value
+          )
+          .subscribe(
+            (res: any) => {
+              if (res.statusCode == 200) {
+                this.toastr.success(res.message);
 
-if(this.newsContent.valid){
-  console.log(this.newsContent.value);
-  if(this.updateNews){
-
-    this.http.patch(environment.baseUrl+'news/'+this.newsId,this.newsContent.value).subscribe((res:any)=>{
-      if( res.statusCode==200){
-        this.toastr.success(res.message)
-
-        this.route.navigate(['/','dashboard','news-list'])
-       }
-    })
-  }else{
-    console.log("news adding", this.images)
-    this.http.post(environment.baseUrl+'news',this.newsContent.value).subscribe((res:any)=>{
-      console.log(res)
-     if( res.statusCode==200){
-      this.toastr.success("News added successfully")
-      this.route.navigate(['/','dashboard','news-list'])
-     }
-    })
+                this.route.navigate(['/', 'dashboard', 'news-list']);
+              }
+            },
+            (error: HttpErrorResponse) => {
+              console.log('error in api ', error);
+              this.toastr.error(error.error.message);
+            }
+          );
+      } else {
+        console.log('news adding', this.images);
+        this.http
+          .post(environment.baseUrl + 'news', this.newsContent.value)
+          .subscribe(
+            (res: any) => {
+              console.log(res);
+              if (res.statusCode == 200) {
+                this.toastr.success('News added successfully');
+                this.route.navigate(['/', 'dashboard', 'news-list']);
+              }
+            },
+            (error: HttpErrorResponse) => {
+              console.log('error in api', error);
+              this.toastr.error(error.error.message);
+            }
+          );
+      }
+    } else {
+      this.toastr.error('Enter All Fields');
+    }
   }
 
-}else{
-  this.toastr.error("Enter All Fields")
-}
-}
-
-ngOnDestroy(): void {
-  this.updateService.news=[]
-}
-
+  ngOnDestroy(): void {
+    this.updateService.news = [];
+  }
 }
