7ef63aacfe9b367833e645fe7c267fe21f23fbaf
Prasadsanglikar <prasads.mobicloud@gmail.com>
2024-04-19 10:11:38 +0530

    enviroment

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -1,6 +1,6 @@
-export const environment = {
+export const environment={
+
   // baseUrl:'http://89.116.122.208/'
-<<<<<<< HEAD
 
   // baseUrl:'https://host.mobicloudtech.com/wuerth-backend/'
   baseUrl:'http://192.168.29.234:3000/'
@@ -12,8 +12,3 @@ export const environment = {
 }
 
 
-=======
-  baseUrl: 'http://192.168.29.234:3000/',
-  // baseUrl:'http://192.168.29.28:3000/'s
-};
->>>>>>> origin/shubham-1
