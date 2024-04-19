1ce7005528f849ead43c62aebd0321f6170d6e2e
vikrant29k <vikrant.mobicloud@gmail.com>
2024-04-19 13:05:13 +0530

    added translation to all pages install package

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,18 +7,42 @@ import { ScoreboardComponent } from './component/scoreboard/scoreboard.component
 import { HeaderComponent } from './component/header/header.component';
 import { NewsUpdateComponent } from './component/news-update/news-update.component';
 import { AngularEditorModule } from '@kolkov/angular-editor';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DashboardMaterialModule } from './modules/dashboard-material.module';
 import { NewsListComponent } from './component/news-list/news-list.component';
 import { ProfileComponent } from './component/profile/profile.component';
 import { DialogAnimationsComponent } from './component/dialog-animations/dialog-animations.component';
-
 import { ImportExcelComponent } from './component/import-excel/import-excel.component';
 import { MatchComponent } from './component/match/match.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 @NgModule({
-  declarations: [DashboardComponent, SliderComponent, ScoreboardComponent, HeaderComponent, NewsUpdateComponent, NewsListComponent, ProfileComponent, DialogAnimationsComponent, ImportExcelComponent, MatchComponent],
+  declarations: [
+    DashboardComponent,
+    SliderComponent,
+    ScoreboardComponent,
+    HeaderComponent,
+    NewsUpdateComponent,
+    NewsListComponent,
+    ProfileComponent,
+    DialogAnimationsComponent,
+    ImportExcelComponent,
+    MatchComponent,
+  ],
   imports: [
-    CommonModule,DashboardRoutingModule,AngularEditorModule,HttpClientModule,DashboardMaterialModule
-  ]
+    CommonModule,
+    DashboardRoutingModule,
+    AngularEditorModule,
+    HttpClientModule,
+    DashboardMaterialModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+        deps: [HttpClient]
+      }
+    }),
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
