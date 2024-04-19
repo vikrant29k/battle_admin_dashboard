c32873a206e7472e9b17abc5abce33e5240f31b1
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-04-19 10:03:09 +0530

    env update

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -1,14 +1,5 @@
-export const environment={
-
+export const environment = {
   // baseUrl:'http://89.116.122.208/'
-<<<<<<< HEAD
-  // baseUrl:'https://host.mobicloudtech.com/wuerth-backend/'
-  baseUrl:'http://192.168.29.234:3000/'
-=======
-  baseUrl:'http://192.168.29.234:3000/'
+  baseUrl: 'http://192.168.29.234:3000/',
   // baseUrl:'http://192.168.29.28:3000/'s
->>>>>>> origin/main
-
-}
-
-
+};
