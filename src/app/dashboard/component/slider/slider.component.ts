335e2f33cbe6c232b80d1c988c8da485dcb0461f
vikrant29k <vikrant.mobicloud@gmail.com>
2024-04-24 11:06:44 +0530

    slider

--- src/app/dashboard/component/slider/slider.component.ts
+++ src/app/dashboard/component/slider/slider.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-
-
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
-  showMenu:boolean=false
-  constructor(private route:Router){
+  showMenu = false;
+  activeMenu: string = localStorage.getItem('activeMenu') || 'import-file'; // Use localStorage for persistence
 
-  }
-  goToRoute(name:string){
-    this.route.navigate(['/','dashboard',name]);
-    this.showMenu = false
+  constructor(private route: Router) { }
+
+  goToRoute(name: string) {
+    this.route.navigate(['/dashboard', name]);
+    this.activeMenu = name;
+    this.showMenu = false;
+    localStorage.setItem('activeMenu', name);
   }
 
   toggleMenu() {
@@ -24,13 +24,8 @@ export class SliderComponent {
   }
 
   logOut() {
-
-    localStorage.removeItem("token")
-
-
     localStorage.removeItem("token");
     localStorage.removeItem('activeMenu')
     this.route.navigate(['/auth/login']);
-
   }
 }
