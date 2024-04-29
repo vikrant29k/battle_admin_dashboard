ea153e89e00e447cf02b0a82704f66ff4520c42c
prasadsang <prasads.mobicloud@gmail.com>
2024-04-29 11:13:23 +0530

    new-update

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -2,12 +2,12 @@ export const environment={
 
   // baseUrl:'http://89.116.122.208/'
 
-  baseUrl:'https://dev.mobicloudtechnologies.com/wuerth-backend/'
+  // baseUrl:'https://dev.mobicloudtechnologies.com/wuerth-backend/'
   // baseUrl:'http://192.168.29.234:3000/'
 
-  // baseUrl:'http://192.168.29.234:3000/'
+  baseUrl:'http://192.168.29.234:3000/'
   // baseUrl:'http://192.168.29.28:3000/'
-
+  // baseUrl:'http://192.168.29.223:3000/'
 
 }
 
