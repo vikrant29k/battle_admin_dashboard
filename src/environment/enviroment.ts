fc8bdd2c09c0e0e686f505daa59b2d3308ff14a9
Shubham Kalamkar <shubham.mobicloud@gmail.com>
2024-05-16 11:14:58 +0530

    env

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -6,9 +6,7 @@ export const environment={
   baseUrl:'http://192.168.29.28:3000/'
 
   // baseUrl:'http://192.168.29.234:3000/'
-  baseUrl:'http://192.168.29.28:3000/'
   // baseUrl:'http://192.168.29.223:3000/'
-
 }
 
 
